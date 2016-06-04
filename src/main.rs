b503c9570b092bdbdf88f906b4f79729aa20bc93
codelusion <ash@aisaac.io>
2016-06-04 10:00:20 -0500

    Concurrency sample code

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::thread;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+fn main() {
+    // initial value to share and mutate
+    // in spawed threads
+    let shared = [1];
+    // use Arc to share data with thread
+    // mutex to allow safe mutation
+    let s = Arc::new(Mutex::new(shared));
+
+    // thread handles
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        // get the inner mutex
+        let mutex = s.clone();
+        // spawn thread and move ownership of mutex
+        let handle = thread::spawn(move || {
+            // lock for mutation inside thread
+            let mut r = mutex.lock().unwrap();
+            r[0] += i;
+            println!("shared: ({} + thread #{}) => {}", r[0] - i, i, r[0]);
+        });
+        // save thread handle to join later
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        // join thread
+        handle.join().unwrap();
+        // what is the current state of the mutex
+        // Could be <locked> or show data
+        println!("{:?}", s);
+    }
+}
